bbb4d8bda748ab608c59b07953337d9b7e6fb384
Eric <gizyatullov0@gmail.com>
2022-09-05 12:42:30 +0400

    '.'

--- messenger/exe/server/setup.py
+++ messenger/exe/server/setup.py
@@ -1,12 +1,33 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
+#
+# setup(name='server_chat_pyqt_gizy',
+#       version='0.0.1',
+#       description='Server packet',
+#       # Будем искать пакеты тут(включаем авто поиск пакетов)
+#       packages=find_packages(),
+#       author_email='gizyatullov0@gmail.com',
+#       author='Eric Gizyatullov',
+#       install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
+#       # зависимости которые нужно до установить
+#       )
 
-setup(name='server_chat_pyqt_June',
-      version='0.0.1',
-      description='Server packet',
-      # Будем искать пакеты тут(включаем авто поиск пакетов)
-      packages=find_packages(),
-      author_email='gizyatullov0@gmail.com',
-      author='Eric Gizyatullov',
-      install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
-      # зависимости которые нужно до установить
-      )
+
+import sys
+
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    'packages': ['common', 'logs', 'server', 'unit_tests']
+}
+
+setup(
+    name='server_chat_pyqt_gizy',
+    version='0.0.1',
+    description='Server',
+    options={
+        'build_exe': build_exe_options
+    },
+    executables=[Executable('server_test.py',
+                            base='Win32GUI',
+                            targetName='server.exe')]
+)
